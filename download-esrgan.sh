e347faedb0b9bab708cd773b9f76fb01f8677e89
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2022-10-31 23:15:27 +0100

    add download-esrgan shell script

---
+++ download-esrgan.sh
@@ -0,0 +1,4 @@
+wget -O  realesrgan.zip https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesrgan-ncnn-vulkan-20220424-ubuntu.zip
+mkdir realesrgan
+unzip realesrgan.zip -d realesrgan
+chmod +x realesrgan/realesrgan-ncnn-vulkan
